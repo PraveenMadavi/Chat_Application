a99633ae99c442afc27ce341d15edea2a72393cc
PraveenMadavi <madavione@gmail.com>
2025-08-10 18:26:07 +0530

    Redis properties edited

--- Backend/src/main/java/com/samvad/chat_app/ChatAppApplication.java
+++ Backend/src/main/java/com/samvad/chat_app/ChatAppApplication.java
@@ -4,8 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableCaching
 public class ChatAppApplication {
 	public static final Logger logger = LoggerFactory.getLogger(ChatAppApplication.class);
 	public static void main(String[] args) {
