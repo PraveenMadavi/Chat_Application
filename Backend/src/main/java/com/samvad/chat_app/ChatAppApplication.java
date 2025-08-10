0d64fbe144ee786e6f1ee7661db0ac26864713e0
PraveenMadavi <madavione@gmail.com>
2025-08-10 18:52:33 +0530

    Redish template edited

--- Backend/src/main/java/com/samvad/chat_app/ChatAppApplication.java
+++ Backend/src/main/java/com/samvad/chat_app/ChatAppApplication.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-//import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
 @EnableCaching
--- Backend/src/main/java/com/samvad/chat_app/configurations/RedisConfig.java
+++ Backend/src/main/java/com/samvad/chat_app/configurations/RedisConfig.java
@@ -7,6 +7,8 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -28,9 +30,16 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+        template.setConnectionFactory(connectionFactory);
+
+        // Use String serializer for keys
+        template.setKeySerializer(new StringRedisSerializer());
+
+        // Use JSON serializer for values
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
         return template;
     }
 }
